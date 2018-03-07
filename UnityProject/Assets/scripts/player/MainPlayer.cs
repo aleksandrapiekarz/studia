9f7c95e9ab4c7a237b276489d8b431b92a4ddc5a
Wojtek <wojtek.kandziak@gmail.com>
2018-03-07 08:53:55 +0100

    Commit
    Dodanie klas

---
+++ UnityProject/Assets/scripts/player/MainPlayer.cs
@@ -0,0 +1,27 @@
+﻿public class MainPlayer {
+
+	private int _Zdrowie;
+	private int _Glod;
+	private int _Woda;
+
+	public MainPlayer() {
+		_Zdrowie = 150;
+		_Glod = 150;
+		_Woda = 150;
+	}
+
+	public int Zdrowie {
+		get { return _Zdrowie; }
+		set { _Zdrowie = value; }
+	}
+
+	public int Glod {
+		get { return _Glod; }
+		set { _Glod = value; }
+	}
+
+	public int Woda {
+		get { return _Woda; }
+		set { _Woda = value; }
+	}
+}
---
+++ UnityProject/Assets/scripts/rabbit/Rabbit.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabbit : MonoBehaviour {
+
+	private MainRabbit _Rabbit = new MainRabbit();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
