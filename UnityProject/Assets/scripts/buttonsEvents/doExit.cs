2619c0cc3d3de605812f91c880163d0982e19105
przemekrogala <36774383+przemekrogala@users.noreply.github.com>
2018-03-10 21:33:48 +0100

    zdarzenie po kliknieciu ExitButton
    wychodzenie z programu po kliknięciu przycisku

---
+++ UnityProject/Assets/scripts/buttonsEvents/doExit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doExit : MonoBehaviour {
+
+	// Use this for initialization
+	public void doQuit(){
+		Debug.Log("has quit game");
+		Application.Quit ();
+	}
+
+
+}
