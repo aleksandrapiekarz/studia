79ea0ddc8b391ac7f10790f2ddc25e6e98bd9ac8
przemekrogala <36774383+przemekrogala@users.noreply.github.com>
2018-03-11 14:27:23 +0100

    przycisk nowaGra
    zdarzenie przycisku Nowa Gra

---
+++ UnityProject/Assets/scripts/buttonsEvents/newGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class newGame : MonoBehaviour {
+
+
+    public void doNewGame()
+    {
+		SceneManager.LoadScene("mainScene");
+
+    }
+    
+}
