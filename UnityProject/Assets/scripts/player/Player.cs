7c53d7b2e0cd538ca95e9508ff850ae8001d0175
Wojtek <wojtek.kandziak@gmail.com>
2018-03-20 16:42:49 +0100

    Dodałem postać.

--- UnityProject/Assets/scripts/player/Player.cs
+++ UnityProject/Assets/scripts/player/Player.cs
@@ -5,14 +5,49 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	private MainPlayer _Player = new MainPlayer();
+	private Animator animator;
+	protected Vector2 direction;
+	float speed = 5f;
 
-	// Use this for initialization
 	void Start () {
 		
+		animator = GetComponent<Animator>();
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		
+		Move ();
+		GetInput ();
+	}
+
+	private void GetInput() {
+
+		direction = Vector2.zero;
+
+		if (Input.GetKey (KeyCode.W)) {
+			direction += Vector2.up;
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			direction += Vector2.left;
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			direction += Vector2.down;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			direction += Vector2.right;
+		}
+	}
+
+	public void Move() {
+		
+		transform.Translate (direction * speed * Time.deltaTime);
+
+		AnimateMovement (direction);
+	}
+
+	public void AnimateMovement(Vector2 direction) {
+
+		animator.SetFloat ("x", direction.x);
+		animator.SetFloat ("y", direction.y);
 	}
 }
