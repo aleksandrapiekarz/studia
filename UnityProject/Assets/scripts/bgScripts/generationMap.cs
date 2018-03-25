86860c68748ddb381dfb2189b9128c33c9bfaa1d
przemekrogala <36774383+przemekrogala@users.noreply.github.com>
2018-03-25 09:17:43 +0200

    Generowanie drzew
    Generowanie drzew, postać nie przechodzi przez drzewa, kamera śledzi postać.
    
    Pogadamy w Poniedziałek...

---
+++ UnityProject/Assets/scripts/bgScripts/generationMap.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generationMap : MonoBehaviour
+{
+
+    // Use this for initialization
+    public GameObject[] _myPrefabs;
+
+    void Start()
+    {
+
+        CreatePrefab();
+    }
+
+    void CreatePrefab()
+    {
+        GameObject clone = Instantiate(_myPrefabs[RandomNumber()]) as GameObject;
+    }
+
+    int RandomNumber()
+    {
+        System.Random rand = new System.Random();
+        return rand.Next(0, _myPrefabs.Length);
+    }
+
+}
---
+++ UnityProject/Assets/treeGenerate/MapGenerate.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerate : MonoBehaviour {
+
+    public GameObject tree;
+    public int number;
+    public int xaxis;
+    public int yaxis;
+    public int zaxis;
+
+    void Start()
+    {
+
+        PlaceTree();
+    }
+    void PlaceTree()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            Instantiate(tree, GeneratedPosition(), Quaternion.identity);
+        }
+    }
+    Vector3 GeneratedPosition()
+    {
+        int x, y, z;
+        x = UnityEngine.Random.Range(-xaxis, xaxis);
+        y = UnityEngine.Random.Range(-yaxis, yaxis);
+        z = UnityEngine.Random.Range(-zaxis, zaxis);
+        return new Vector3(x, y, z);
+
+    }
+}
