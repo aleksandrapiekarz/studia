78325a24e5e19ebac5abf5ccdc7251cad60432a5
Wojtek <wojtek.kandziak@gmail.com>
2018-03-27 14:50:48 +0200

    Zapis stanu gry.

--- UnityProject/Assets/scripts/buttonsEvents/loadBtn.cs
+++ UnityProject/Assets/scripts/buttonsEvents/loadBtn.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class loadBtn : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-
-
     public Text theText;
-    // Use this for initialization
-    public void doQuit()
-    {
-        
+    
+    public void doLoad()
+	{
+		float pX = PlayerPrefs.GetFloat ("PlayerX");
+		float pY = PlayerPrefs.GetFloat ("PlayerY");
+		transform.position = new Vector2 (pX, pY);
+
+		Random.seed = PlayerPrefs.GetInt ("Random");
+		MapGenerate _Map = new MapGenerate();
+
+		SceneManager.LoadScene("mainScene");
     }
 
     public void OnPointerEnter(PointerEventData eventData)
---
+++ UnityProject/Assets/scripts/buttonsEvents/zapiszWczytaj.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zapiszWczytaj : MonoBehaviour {
+
+	public void zapisz() {
+
+		PlayerPrefs.SetFloat ("PlayerX", transform.position.x);
+		PlayerPrefs.SetFloat ("PlayerY", transform.position.y);
+
+		PlayerPrefs.SetInt ("Random", Random.seed);
+	}
+		
+	public void wczytaj() {
+
+		float pX = PlayerPrefs.GetFloat ("PlayerX");
+		float pY = PlayerPrefs.GetFloat ("PlayerY");
+
+		transform.position = new Vector2 (pX, pY);
+
+		Random.seed = PlayerPrefs.GetInt ("Random");
+
+		MapGenerate _Map = new MapGenerate();
+	}
+}
