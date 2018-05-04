9150b2472da9b8eb29d1192b696161464e77b36d
przemekrogala <36774383+przemekrogala@users.noreply.github.com>
2018-05-04 13:50:04 +0200

    Oprogramowanie wilka
    Wilk biega za graczem, gdy ten znajdzie się w jego zasięgu. Gdy gracz ucieknie (oddali sie na pewną odległość  to wilk zatrzymuje się.

---
+++ UnityProject/Assets/scripts/wolf/EnemyAI.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : Player {
+	private Transform target;
+	public int moveSpeed;
+	public int rotationSpeed;
+
+	public Transform Target
+	{
+		get{
+			return target;
+		}
+
+
+		set {
+
+
+			target = value;
+		}
+
+
+
+	}
+
+
+
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () { 
+
+		FollowTarget ();
+
+	}
+
+
+	private void FollowTarget()
+	{
+		if (target != null) {
+
+			transform.position=Vector2.MoveTowards(transform.position, target.position, 1*Time.deltaTime);
+
+		}
+
+
+	
+
+
+
+			}
+
+}
---
+++ UnityProject/Assets/scripts/wolf/Range.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Range : MonoBehaviour {
+
+
+	private EnemyAI parent;
+	// Use this for initialization
+	void Start () {
+		parent = GetComponentInParent<EnemyAI> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Player")
+		{
+
+
+			parent.Target = collision.transform;
+
+		}
+
+	}
+	private void OnTriggerExit2D(Collider2D collision)
+
+	{
+
+		if (collision.tag == "Player") {
+
+			parent.Target = null;
+		}
+
+	}
+
+
+
+}
+
