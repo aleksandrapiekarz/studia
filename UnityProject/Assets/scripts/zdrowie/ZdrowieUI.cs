af719a66d3d537321324ac3ce0fda29ef014ba93
Wojtek <wojtek.kandziak@gmail.com>
2018-05-08 21:31:11 +0200

    test

---
+++ UnityProject/Assets/scripts/zdrowie/ZdrowieUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZdrowieUI : MonoBehaviour {
+
+	float maxZdrowie;
+	float obecneZdrowie;
+	public GUISkin skin;
+
+	void Start () {
+		maxZdrowie = 100;
+		obecneZdrowie = 100;
+	}
+
+	void Update () {
+		ognisko ();
+	}
+
+	void OnGUI() {
+		GUI.Box (new Rect (50, Screen.height - 50, 150, 20), "Zdrowie: " + obecneZdrowie + "/" + maxZdrowie, skin.GetStyle("PasekZdrowia"));
+	}
+
+	public void ognisko() {
+		if((transform.position.x > -6 && transform.position.y < 1) && (transform.position.x < 1 && transform.position.y > -5))
+			obecneZdrowie = 100;
+		else 
+			obecneZdrowie -= Time.deltaTime / 3;
+	}
+}
